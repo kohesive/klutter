99397e24cabea4e62acf19a98a3dbc8a7fff68fa
apatrida <jayson.minard@gmail.com>
2016-09-16 15:23:32 -0300

    formatting

--- core/src/main/kotlin/uy/klutter/core/common/Numbers.kt
+++ core/src/main/kotlin/uy/klutter/core/common/Numbers.kt
@@ -30,8 +30,8 @@ fun Double.coerce(minVal: Double, maxVal: Double): Double = this.minimum(minVal)
 fun Long.humanReadable(): String {
     if (this <= 0) return "0"
     val units = arrayOf("B", "KB", "MB", "GB", "TB", "EB")
-    val digitGroups = (Math.log10(this.toDouble())/Math.log10(1024.0)).toInt();
-    return DecimalFormat("#,##0.#").format(this/Math.pow(1024.0, digitGroups.toDouble())) + " " + units[digitGroups];
+    val digitGroups = (Math.log10(this.toDouble())/Math.log10(1024.0)).toInt()
+    return DecimalFormat("#,##0.#").format(this/Math.pow(1024.0, digitGroups.toDouble())) + " " + units[digitGroups]
 }
 
 fun Int.humanReadable(): String {
--- core/src/main/kotlin/uy/klutter/core/common/Paths.kt
+++ core/src/main/kotlin/uy/klutter/core/common/Paths.kt
@@ -1,10 +1,8 @@
 package uy.klutter.core.common
 
-
 import java.nio.file.Files
 import java.nio.file.Path
 
 fun Path.exists(): Boolean = Files.exists(this)
 fun Path.notExists(): Boolean = !this.exists()
-
 fun Path.deleteRecursively(): Boolean = this.toFile().deleteRecursively()
